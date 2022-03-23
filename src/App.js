569723ef90fdc5e00532cba98aea2b4cff9e90d9
vandinh124 <vanvdinh124@gmail.com>
2022-03-23 16:14:13 -0700

    added Home and fetch movies to home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //Components
 import Header from './components/Header/Header';
-
+import Home from './components/Home/Home';
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
+      <Home />
       Start here.
       <GlobalStyle />
     </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+//Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../../config';
+//API
+import API from '../../API';
+//image
+import NoImage from '../../images/no_image.jpg';
+
+const Home = () => {
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  console.log('state', state);
+  return <div>Home Page</div>;
+};
+
+export default Home;
