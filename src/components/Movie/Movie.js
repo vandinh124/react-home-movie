9b85f4499910a582cbc96ac2cd6ee1de7f02e077
vandinh124 <vanvdinh124@gmail.com>
2022-03-25 10:46:59 -0700

    added actors grid

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -8,6 +8,7 @@ import Spinner from '../Spinner/Spinner';
 import BreadCrumb from '../BreadCrumb/BreadCrumb';
 import MovieInfo from '../MovieInfo/MovieInfo';
 import MovieInfoBar from '../MovieInfoBar/MovieInfoBar';
+import Actor from '../Actor/Actor';
 //Hook
 import useMovieFetch from '../../hooks/useMovieFetch';
 //Image
@@ -31,6 +32,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header='Actors'>
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          ></Actor>
+        ))}
+      </Grid>
     </>
   );
 };
